a6c1a31ae17f5490b598933e59e7ece9a0821f6a
Kaydreanna Castaneda <kaydreannac@icloud.com>
2025-03-28 12:50:06 -0600

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using PlanToPlate.Views;
+
+namespace PlanToPlate
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+            var mainPage = new LoginPage();
+            var navPage = new NavigationPage(mainPage);
+            MainPage = navPage;
+        }
+
+        //protected override Window CreateWindow(IActivationState? activationState)
+        //{
+        //    return new Window(new AppShell());
+        //}
+    }
+}
