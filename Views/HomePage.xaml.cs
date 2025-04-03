772fdfec84c3cc879011b3ba8cfc2fa4d8799745
Kaydreanna Castaneda <kaydreannac@icloud.com>
2025-04-03 12:12:52 -0600

    Update UI and navigation across multiple pages
    - Revamped color scheme in Colors.xaml with new values for Primary, Secondary, and related keys.
    - Replaced welcome labels with specific titles on HomePage, RecipesPage, ScheduleMealsPage, SettingsPage, and ShoppingListPage.
    - Introduced grid layouts with navigation buttons on HomePage, RecipesPage, ScheduleMealsPage, SettingsPage, and ShoppingListPage.
    - Added Login button to LoginPage with corresponding event handling for navigation to HomePage.
    - Implemented navigation button click handlers in HomePage.xaml.cs, RecipesPage.xaml.cs, ScheduleMealsPage.xaml.cs, SettingsPage.xaml.cs, and ShoppingListPage.xaml.cs to facilitate page transitions.

--- Views/HomePage.xaml.cs
+++ Views/HomePage.xaml.cs
@@ -9,4 +9,36 @@ public partial class HomePage : ContentPage
 		InitializeComponent();
 	}
 
+
+    #region Nav Bar
+    private void recipesButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new RecipesPage());
+        Navigation.RemovePage(this);
+    }
+
+    private void shoppingListButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ShoppingListPage());
+        Navigation.RemovePage(this);
+    }
+
+    private void homeButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new HomePage());
+        Navigation.RemovePage(this);
+    }
+
+    private void scheduleMealsButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ScheduleMealsPage());
+        Navigation.RemovePage(this);
+    }
+
+    private void settingsButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SettingsPage());
+        Navigation.RemovePage(this);
+    }
+    #endregion
 }
--- Views/RecipesPage.xaml.cs
+++ Views/RecipesPage.xaml.cs
@@ -6,4 +6,37 @@ public partial class RecipesPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+
+    #region Nav Bar
+    private void recipesButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new RecipesPage());
+        Navigation.RemovePage(this);
+    }
+
+    private void shoppingListButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ShoppingListPage());
+        Navigation.RemovePage(this);
+    }
+
+    private void homeButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new HomePage());
+        Navigation.RemovePage(this);
+    }
+
+    private void scheduleMealsButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new ScheduleMealsPage());
+        Navigation.RemovePage(this);
+    }
+
+    private void settingsButton_Clicked(object sender, EventArgs e)
+    {
+        Navigation.PushAsync(new SettingsPage());
+        Navigation.RemovePage(this);
+    }
+    #endregion
 }
