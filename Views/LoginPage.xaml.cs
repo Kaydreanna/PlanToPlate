6f8dbb3181076aff49461ee06b16e79bbf6b541b
Kaydreanna Castaneda <kaydreannac@icloud.com>
2025-04-10 09:28:31 -0600

    Implement log in vs create account logic
    -Updated `LoginPage.xaml` to initially hide email and confirm password fields, and added a new button for account creation.
    -Enhanced `LoginPage.xaml.cs` with logic to toggle between login and account creation modes, including visibility management for relevant fields and labels.

--- Views/LoginPage.xaml.cs
+++ Views/LoginPage.xaml.cs
@@ -2,6 +2,7 @@ namespace PlanToPlate.Views;
 
 public partial class LoginPage : ContentPage
 {
+    private bool createAccount = false;
 	public LoginPage()
 	{
 		InitializeComponent();
@@ -11,5 +12,40 @@ public partial class LoginPage : ContentPage
     {
         await Navigation.PushAsync(new HomePage());
         Navigation.RemovePage(this);
+    }
+
+    private async void createAccountButton_Clicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new HomePage());
+        Navigation.RemovePage(this);
+    }
+
+    private void createAccountLink_Clicked(object sender, EventArgs e)
+    {
+        if(createAccount == false)
+        {
+            createAccount = true;
+            emailLabel.IsVisible = true;
+            emailEntry.IsVisible = true;
+            confirmPasswordLabel.IsVisible = true;
+            confirmPasswordEntry.IsVisible = true;
+            loginLabel.Text = "Create Account";
+            LoginButton.IsVisible = false;
+            createAccountButton.IsVisible = true;
+            createAccountLink.Text = "Already have an account? Log in";
+        }
+        else
+        {
+            createAccount = false;
+            emailLabel.IsVisible = false;
+            emailEntry.IsVisible = false;
+            confirmPasswordLabel.IsVisible = false;
+            confirmPasswordEntry.IsVisible = false;
+            loginLabel.Text = "Log in";
+            LoginButton.IsVisible = true;
+            createAccountButton.IsVisible = false;
+            createAccountLink.Text = "Don't have an account? Create one";
+        }
+
     }
 }
